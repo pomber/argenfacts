e22e5de1bd8970869655464f8c9ed37f9d1591f5
pomber <pombopombopombo@gmail.com>
2017-05-13 16:51:18 -0300

    Height

---
+++ src/viz-display.js
@@ -0,0 +1,13 @@
+		class VizDisplay extends Polymer.Element {
+			static get is() { return 'viz-display'; }
+      static get properties() {
+        return {
+          prop1: {
+            type: String,
+            value: 'viz-display'
+          }
+        };
+      }
+		}
+
+		window.customElements.define(VizDisplay.is, VizDisplay);
